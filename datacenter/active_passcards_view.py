05b4a193f88d4a3790f87c8bff11fb283ac8178b
Dmitriyrevilov <dimrevilov@gmail.com>
2025-03-09 14:38:02 +0500

    new commit

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    posts = Passcard.objects.all()
-    activate_passcards = posts.filter(is_active=True)
+    active_passcards = Passcard.objects.filter(is_active=True)
     context = {
-        "active_passcards": activate_passcards, 
+        "active_passcards": active_passcards
     }
     return render(request, "active_passcards.html", context)
--- datacenter/get_duration.py
+++ datacenter/get_duration.py
@@ -1,23 +1,27 @@
 from django.utils.timezone import localtime
 
+SECONDS_PER_HOUR = 3600
+SECONDS_PER_MINUTE = 60
+LONG_VISIT_THRESHOLD_SECONDS = 600
 
 
 def get_duration(visit):
     time_now = localtime()
-    if not visit.leaved_at:
+    if visit.leaved_at is None:
         time_in_storage = time_now - visit.entered_at
+        return time_in_storage
+    else:
+        time_in_storage = visit.leaved_at - visit.entered_at
         return time_in_storage
 
 
 def format_duration(duration):
-    hours = duration.seconds // 3600
-    minutes = (duration.seconds % 3600) // 60
-    seconds = duration.seconds % 60
+    total_seconds = int(duration.total_seconds())
+    hours = total_seconds // SECONDS_PER_HOUR
+    minutes = (total_seconds % SECONDS_PER_HOUR) // SECONDS_PER_MINUTE
+    seconds = total_seconds % SECONDS_PER_MINUTE
     return f"{hours:02}:hours  {minutes:02}:minutes {seconds}:seconds"
 
 
 def is_visit_long(visit):
-    if get_duration(visit).total_seconds() > 600:
-        return True
-    else:
-        return False
+    return not get_duration(visit).total_seconds() > LONG_VISIT_THRESHOLD_SECONDS
