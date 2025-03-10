4eddf820767b4ced3545e7fe4b93d9417afb8ca2
Dmitriyrevilov <dimrevilov@gmail.com>
2025-03-10 15:00:16 +0500

    Убрал is None

--- datacenter/get_duration.py
+++ datacenter/get_duration.py
@@ -7,7 +7,7 @@ LONG_VISIT_THRESHOLD_SECONDS = 600
 
 def get_duration(visit):
     time_now = localtime()
-    if visit.leaved_at is None:
+    if visit.leaved_at:
         time_in_storage = time_now - visit.entered_at
         return time_in_storage
     else:
